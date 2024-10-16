308716336bd1eb177d93c0bcd322e3da891566d3
jimmie <jkkimunyi@gmail.com>
2024-10-16 20:55:49 +0300

    Updated main function to use brrightdata scrapper

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from scrape import scrape_website_with_brightdata, custon_webscrapper
 
 st.title("AI Web Scrapper")
 url = st.text_input("Enter website URL")
 
 if st.button("Scrape site"):
     st.write("Scrapping the website ...")
+    result = scrape_website_with_brightdata(url)
+    print(result)
