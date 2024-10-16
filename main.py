043136150994f02383e1ff3e97ca10b2f12be014
jimmie <jkkimunyi@gmail.com>
2024-10-16 21:17:18 +0300

    Updated main.py to call scape.py methods

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website_with_brightdata, custon_webscrapper
+from scrape import scrape_website_with_brightdata, custom_webscrapper, split_dom_content,clean_body_content, extract_body_content
 
 st.title("AI Web Scrapper")
 url = st.text_input("Enter website URL")
@@ -7,4 +7,11 @@ url = st.text_input("Enter website URL")
 if st.button("Scrape site"):
     st.write("Scrapping the website ...")
     result = scrape_website_with_brightdata(url)
-    print(result)
+    
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
