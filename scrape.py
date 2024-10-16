1249a9a47a0ff2fa4e14606f8ab63f31083856e5
jimmie <jkkimunyi@gmail.com>
2024-10-16 20:54:57 +0300

    Added an extract and cleane functins to extract and clean data

--- scrape.py
+++ scrape.py
@@ -46,3 +46,22 @@ def scrape_website_with_brightdata(website):
         print('Navigated! Scraping page content...')
         html = driver.page_source
         print(html)
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content():
+    soup = BeautifulSoup()
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if lines.strip()
+    )
+    
